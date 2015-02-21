2225f0324c0497b5138caf1ed53b22d6222cad99
ElenaMGR <elenamgori@gmail.com>
2015-02-21 16:46:50 +0100

    Modificación ActivityConfig

--- app/src/main/java/es/mresti/alhambeers/ConfigActivity.java
+++ app/src/main/java/es/mresti/alhambeers/ConfigActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 
+import com.gc.materialdesign.views.ButtonRectangle;
+
 
 /**
  * Created by Elena on 21/02/2015.
@@ -24,5 +26,24 @@ public class ConfigActivity extends Activity {
     public void btnSelecTwitter (View v){
         Intent i = new Intent(this,SelecTwitterActivity.class);
         startActivity(i);
+    }
+
+    public void btnZona (View v){
+        Intent i = new Intent(this,InfoZona.class);
+        startActivity(i);
+    }
+
+    public void btnBloquear (View v){
+        // Vinculamos el Boton con la variable
+        ButtonRectangle bloquear = (ButtonRectangle) findViewById(R.id.btn_bloquear);
+
+        //Asignamos el texto que corresponda a las instrucciones
+        if (bloquear.getText().equals(R.string.bloquear)) {
+            bloquear.setText(getResources().getString(R.string.desbloquear));
+        }
+        else{
+            bloquear.setText(getResources().getString(R.string.bloquear));
+        }
+
     }
 }
