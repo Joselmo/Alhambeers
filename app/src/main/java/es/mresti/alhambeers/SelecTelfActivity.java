3a7da3db7177047dfa58d22435ae2b3b042b15f4
ElenaMGR <elenamgori@gmail.com>
2015-02-21 20:11:54 +0100

    modificacion Preferencias

--- app/src/main/java/es/mresti/alhambeers/SelecTelfActivity.java
+++ app/src/main/java/es/mresti/alhambeers/SelecTelfActivity.java
@@ -1,7 +1,11 @@
 package es.mresti.alhambeers;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by asus on 21/02/2015.
@@ -11,5 +15,24 @@ public class SelecTelfActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telf);
+    }
+
+    public void aniadirTelf (View v) {
+
+        EditText etTelf = (EditText) findViewById(R.id.etTelefono);
+
+        SharedPreferences prefs =
+                getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("tlf", etTelf.getText().toString());
+        editor.commit();
+
+
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(
+                        AndroidPrefScreensActivity.this);
+
+        Log.i("", "Opción 1: " + pref.getString("tlf"));
     }
 }
