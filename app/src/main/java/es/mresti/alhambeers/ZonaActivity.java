83076aa09fde0dcc86436aa54b31226a47ec6c4e
Joselmo <joselmo@correo.ugr.es>
2015-02-21 21:30:07 +0100

    Conexion GPS
    "implementacion" del GPS

--- app/src/main/java/es/mresti/alhambeers/ZonaActivity.java
+++ app/src/main/java/es/mresti/alhambeers/ZonaActivity.java
@@ -2,8 +2,14 @@ package es.mresti.alhambeers;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.gc.materialdesign.widgets.ProgressDialog;
+
+import org.apache.http.NameValuePair;
 
 
 public class ZonaActivity extends Activity {
@@ -18,6 +24,53 @@ public class ZonaActivity extends Activity {
     Intent i = new Intent(this,MainActivity.class);
     startActivity(i);
   }
+
+    public void btnGetPosition (View v){
+        Async_Consulta sal = new Async_Consulta();
+        sal.execute();
+
+        Intent i = new Intent(this,DistActivity.class);
+        startActivity(i);
+    }
+
+
+    class Async_Consulta extends AsyncTask<String, String, String> {
+        private ProgressDialog pDialog;
+
+        protected void onPreExecute() {
+            pDialog = new ProgressDialog(ZonaActivity.this,"Obteniendo posición...");
+
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... params) {
+            String result = "noexiste";
+            //Lanzamos la consulta al servidor GPS
+            // Recogemos los datos del servidor y se lo pasamos a la capa de java
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return "";
+        }
+
+        protected void onPostExecute(String result) {
+            // Recogemos los datos del servidor y ya podemos utilizarlos en la app
+            // Mediante la API REST
+            //
+            pDialog.dismiss();
+            if (result.equals("noexiste")) {
+                Toast.makeText(getApplicationContext(),
+                        "Error al obtener posicionamiento",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
 
   
 }
