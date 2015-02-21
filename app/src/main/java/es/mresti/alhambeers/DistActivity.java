66cdaefd58085684c463c877b766f2809ac745f6
Esteban Dorado Roldan <fadoro@gmail.com>
2015-02-21 20:16:36 +0100

    Añadido Toast para guardar la zona

--- app/src/main/java/es/mresti/alhambeers/DistActivity.java
+++ app/src/main/java/es/mresti/alhambeers/DistActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gc.materialdesign.views.ButtonRectangle;
 
@@ -41,6 +42,10 @@ public class DistActivity extends Activity {
     }
   }
   public void btnOk (View v){
+
+    String texto = getResources().getString(R.string.toast_save);
+    Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_LONG).show();
+
     Intent i = new Intent(this,MainActivity.class);
     startActivity(i);
   }
